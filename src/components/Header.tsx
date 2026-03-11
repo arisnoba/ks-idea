2e8cc468271f7c45256942ab59e8ff9c4f06d675
arisnoba <arisnoba@gmail.com>
2026-03-11 18:53:27 +0900

    refactor: Update header logo to use Next.js Image and Link, refine snap scroll behavior, and adjust footer font sizes.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,8 @@
 'use client';
 
 // import { useState } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
 import styles from './Header.module.scss';
 
 export default function Header() {
@@ -21,9 +23,9 @@ export default function Header() {
 	return (
 		<header className={`header ${styles.header}`}>
 			<nav className={`container ${styles.nav}`}>
-				<a href="#" className={styles.logo}>
-					<img src="/images/logo.svg" alt="KS IDEA" />
-				</a>
+				<Link href="/" className={styles.logo}>
+					<Image src="/images/logo.svg" alt="KS IDEA" width={120} height={40} />
+				</Link>
 
 				{/* Desktop nav */}
 				{/* <ul className={styles.navMenu}>
